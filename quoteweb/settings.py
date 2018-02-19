d6d777d4b915afabe0864670215caf631552e3d2
Amin Memon <me@aminmemon.com>
2018-02-19 18:25:19 +0530

    Update settings.py

--- quoteweb/settings.py
+++ quoteweb/settings.py
@@ -109,11 +109,11 @@ ROOT_URLCONF = 'quoteweb.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'quoteweb.wsgi.application'
 
-TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__) +'/templates')
+TEMPLATE_DIRS = [(os.path.join(os.path.dirname(__file__) +'/templates')
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+)]
 
 INSTALLED_APPS = (
     'django.contrib.auth',
